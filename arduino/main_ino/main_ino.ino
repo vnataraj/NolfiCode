8ac68a25eef84e7dd8fc1abd40e5d883d1ae044b
Vipul Nataraj <vnataraj@purdue.edu>
2013-10-26 02:29:03 -0400

    added IR code into main file, created helper functions for each of the code blocks

--- arduino/main_ino/main_ino.ino
+++ arduino/main_ino/main_ino.ino
@@ -1,17 +1,19 @@
 #include <Servo.h>
 
-Servo servo;
+Servo caudal;
 int pos=0;
+int IR_pin=1;
+int caudal_pin=13;
 
 void setup(){
 	Serial.begin(9600);
-	servo.attach(13);
+	caudal.attach(caudal_pin);
 	Serial.println("Okay, serial is instantiated");
 }
 
 void loop(){
   //servo.write(90);
-  for(pos=0; pos<=180; pos+=180){
+  /*for(pos=0; pos<=180; pos+=180){
    servo.write(pos);
    delay(100);
    Serial.println("forwards loop");
@@ -22,5 +24,25 @@ void loop(){
    delay(100);
    Serial.println("backwards loop"); 
   }
+ */
+	writeToCaudalServo(caudal);
   
+}
+
+void writeToCaudalServo(Servo servo){
+	for(pos=0; pos<=180; pos+=180){
+		servo.write(pos);
+		delay(100);
+	}
+	for(pos=180; pos>=0; pos-=180){
+		servo.write(pos);
+		delay(100);
+	}
+}
+
+void readIRSensor(int pin){
+	float volts = analogRead(pin)*0.0048828125;
+	float distance = 65*pow(volts, -1.10);
+	Serial.println(distance);
+	delay(100);
 }
