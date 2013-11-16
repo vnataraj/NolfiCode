3873210c27706297ab6e31fe3683a629925ed564
Vipul Nataraj <vnataraj@purdue.edu>
2013-11-16 16:55:21 -0500

    trying to get FTDI to work on windows

--- arduino/main_ino/main.h
+++ arduino/main_ino/main.h
@@ -1,12 +1,11 @@
 
 /* DEFINE PIN CONSTANTS HERE */
 
-#define IR_PIN1				1 // IR sensor (1) signal pin
-#define IR_PIN2				2
-#define GYRO_PIN			3
-#define FORCE_PIN			4
+#define IR_PIN1			        16 // IR sensor (1) signal pin
+#define IR_PIN2				3
+#define GYRO_PIN			4
 #define DORSAL_PIN			10
-#define PECTORAL_LEFT_PIN	11
-#define PECTORAL_RIGHT_PIN	12
-#define CAUDAL_PIN			13 // caudal motor signal pin
+#define PECTORAL_LEFT_PIN	        11
+#define PECTORAL_RIGHT_PIN	        12
+#define CAUDAL_PIN			9 // caudal motor signal pin
 
--- arduino/main_ino/main_ino.ino
+++ arduino/main_ino/main_ino.ino
@@ -18,20 +18,31 @@ Servo dorsal;
 bool left = false;
 bool right = false;
 bool distance1 = false;
+int count=0;
 bool distance2 = false;
 bool up = false;
 bool down = false;
+float angles[3]; // yaw/pitch/roll
+//float saved[3]; //saved yaw/pitch/roll
+
+FreeSixIMU sixDOF = FreeSixIMU();
 
 /* SETUP ROUTINE */
 
 void setup(){
 	sei();  // enable interrupts
 	Serial.begin(9600);
+       // Wire.begin();
 	pinMode(IR_PIN1, INPUT);
-	pinMode(IR_PIN2, INPUT);
-	//PCIntPort::attachInterrupt(IR_PIN1, actOnDistance1, CHANGE);
+	//pinMode(IR_PIN2, INPUT);
+        Serial.println("Init 6DOF");
+       // sixDOF.init();
+	//PCintPort::attachInterrupt(IR_PIN1, &actOnDistance1, FALLING);
 	//PCIntPort::attachInterrupt(IR_PIN2, actOnDistance2, CHANGE);
 	caudal.attach(CAUDAL_PIN);
+        Serial.println("Init complete");
+        //pectoral1.attach(PECTORAL_LEFT_PIN);
+        //pectoral2.attach(PECTORAL_RIGHT_PIN);
 	Serial.println("Okay, serial is instantiated");
 }
 
@@ -39,8 +50,10 @@ void setup(){
 
 void actOnDistance1(){
 	distance1=true;
+        count++;
+        //Serial.println("distance1");
 }
-void actOnDistance2(){
+/*void actOnDistance2(){
 	distance2=true;
 }
 
@@ -51,78 +64,134 @@ void actOnLeft(){
 void actOnRight(){
 	right=true;
 }
+*/
 
 /* MAIN EXECUTION LOOP */
-
 void loop(){
+        int val = analogRead(IR_PIN1);
+        Serial.println(val);
+        //float volts = analogRead(IR_PIN1)*0.0048828125;
+        //float distance = 65*pow(volts, -1.10); 
+	//Serial.println(distance);
+        //Serial.println("hello");
+        /*
+        sixDOF.getAngles(angles);
+        Serial.print(angles[0]);
+        Serial.print(" | ");
+        Serial.print(angles[1]);
+        Serial.print(" | ");
+        Serial.print(angles[2]);
+        Serial.println("");
+        delay(5);
+        */
 	startServoRoutine();
+        
+       //caudal.write(140);
 }
 
 /* MOTOR FUNCTION */
 
 void startServoRoutine(){
-	if(distance1 && distance2){
-		//turn right();
+	if(distance1){
+		//turn right();  //dorsal
 		distance1=false;
-		distance2=false;
+		//writeToDorsalServo(caudal);
+                Serial.println(count);
 	}
+/*
 	if(left){
-		//tilt right();
+		//tilt right(); //one pectoral
 		left=false;
 	}
 	else if(right){
-		//tilt left();
+		//tilt left(); // other pectoral
 		right=false;
 	}
 	if(up){
-		//tilt down();
+		//tilt down(); //both pectorals
 	}
 	else if(down){
-		//tilt up();
+		//tilt up();  //both pectorals
 	}
-	writeToCaudalServo(caudal);
-	readIRSensor(IR_PIN1, IR_PIN2);
+        */
+        //caudal.write(0);
+        //caudal.write(95);
+        writeToCaudalServo(caudal);
+        //writeToPectoralServo(pectoral1, pectoral2);
+        //writeToPectoralServoTest(caudal);
+        //writeToDorsalServo(caudal);
+	//readIRSensor(IR_PIN1, IR_PIN2);
 	//readIRSensor(IR_PIN1);   // USE FOR TESTING PURPOSES
 }
 
 
 /* INDIVIDUAL MOTOR FUNCTIONS */
 void writeToCaudalServo(Servo servo){
-	int pos=0;
-	for(pos=0; pos<=180; pos+=180){
+	int pos=16;
+	for(pos=16; pos<=180; pos+=166){
 		servo.write(pos);
-		delay(100);
+		delay(200);
 	}
-	for(pos=180; pos>=0; pos-=180){
+	for(pos=180; pos>=16; pos-=166){
 		servo.write(pos);
-		delay(100);
+		delay(200);
 	}
 }
 
-void writeToPectoralServo(Servo right_servo, Servo left_servo){ // STILL NEED TO DETERMINE OPTIMAL MOTOR FREQUENCY 
-	int pos=0;
-	for(pos=0; pos<=180; pos+=180){
-		left_servo.write(pos);
-		delay(100);
-	}
-	for(pos=0; pos<=180; pos+=180){
-		right_servo.write(pos);
-		delay(100);
-	}
+void writeToPectoralServoTest(Servo servo){
+ sixDOF.getAngles(angles);
+ Serial.println(angles[1]);
+ if(angles[1]<328 && angles[1]>180){
+   servo.write(180);
+   Serial.println("up");
+   delay(1000);
+ }
+ else if(angles[1]>53 && angles[1]<180){
+  servo.write(0);
+  Serial.println("down");
+   delay(1000);
+ }
+ servo.write(90);
+ delay(1000); 
+}
+
+void writeToPectoralServo(Servo right_servo, Servo left_servo){
+        sixDOF.getAngles(angles); // STILL NEED TO DETERMINE OPTIMAL MOTOR FREQUENCY
+        if(angles[1]<339 && angles[1]>180){
+          left_servo.write(180);
+          right_servo.write(0);
+          delay(100);
+        }
+        else if(angles[1]>30 && angles[1]<180){
+          left_servo.write(0);
+          right_servo.write(180);
+          delay(100);
+        }
+        else{
+         left_servo.write(90);
+         right_servo.write(90);
+         delay(100); 
+        }
+        //swriteToCaudalServo(caudal);
+        
 }
 
 void writeToDorsalServo(Servo servo){
-	int pos = 0;
-	for(pos = 0; pos <=180 ; pos +=180){
-		servo.write(pos);
-                delay(100);
-	}
-	for(pos=0; pos<=180; pos+=180){
-		servo.write(pos);
-	}
+        int pos=100;
+        for(pos=100; pos<=140; pos+=20){
+          servo.write(pos);
+          delay(100);
+        }
+        for(pos=140; pos>=100; pos-=20){
+          servo.write(pos);
+          delay(100);
+        }
+	//int pos = 180;
+	//servo.write(pos);
 }
 
 /* INDIVIDUAL IR FUNCTION */
+/*
 void readIRSensor(int pin, int pin2){ // READ IR SENSOR 
 	float volts = analogRead(pin)*0.0048828125; // trimming raw data(low) into voltage from signal
 	float distance = 65*pow(volts, -1.10); // converting signal to distance
@@ -132,9 +201,10 @@ void readIRSensor(int pin, int pin2){ // READ IR SENSOR
 	/*if(distance < 500 && distance2 < 500){
 		
 	}
-        */
+        /
 	delay(100);
 }
+*/
 
 /* void readIRSensor(int pin){
 	float volts = analogRead(pin)*0.0048828125;
